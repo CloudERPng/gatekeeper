4696158d0d4f51716c176ddd81adedd6ddb8ab65
tunde <tundebabzy@gmail.com>
2020-02-12 23:23:02 +0100

    implement login page

--- src/views/LoginForm.vue
+++ src/views/LoginForm.vue
@@ -6,22 +6,20 @@
       </v-card-title>
       <v-card-text>
         <v-form>
-          <v-text-field
-            label="Username"
-            prepend-icon="mdi-account-circle"
-          />
+          <v-text-field label="Username" prepend-icon="mdi-account-circle" v-model="username" />
           <v-text-field
             :type="showPassword ? 'text' : 'password'"
             label="Password"
             prepend-icon="mdi-lock"
             :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
             @click:append="showPassword = !showPassword"
+            v-model="password"
           />
         </v-form>
       </v-card-text>
       <v-divider></v-divider>
       <v-card-actions>
-        <v-btn color="info">Login</v-btn>
+        <v-btn color="info" @click="login">Login</v-btn>
       </v-card-actions>
     </v-card>
   </v-app>
@@ -29,12 +27,32 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import Axios from 'axios';
 
 export default Vue.extend({
   name: 'LoginForm',
 
   data: () => ({
     showPassword: false,
+    username: '',
+    password: '',
   }),
+
+  methods: {
+    async login() {
+      try {
+        const response = await Axios.post('http://127.0.0.1:3000/login', {
+          username: this.username,
+          password: this.password,
+        });
+
+        if (response.status === 200) {
+          this.$router.push({ name: 'Home' });
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
 });
 </script>
