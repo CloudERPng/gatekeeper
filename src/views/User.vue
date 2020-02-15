e13d016d28ab4bcfbffc61ac0cf3381ca0400dd4
tunde <tundebabzy@gmail.com>
2020-02-15 15:24:19 +0100

    - implement form validation - implement user save - redirect to user list if save is successful

--- src/views/User.vue
+++ src/views/User.vue
@@ -14,13 +14,13 @@
         <v-container fluid>
           <v-row align="center" justify="center">
             <v-col cols="8">
-              <v-form ref="form" v-model="valid" lazy-validation>
+              <v-form ref="form" v-model="valid">
                 <v-select
                   v-model="customer"
                   :items="customers"
                   item-text="name"
                   item-value="id"
-                  :rules="[v => !!v || 'Item is required']"
+                  :rules="customerRules"
                   label="Customer"
                   required
                 ></v-select>
@@ -32,10 +32,12 @@
                 </v-text-field>
 
                 <v-text-field
-                  v-if="is_superuser"
+                  v-show="is_superuser"
                   v-model="password"
                   :rules="passwordRules"
-                  label="Password">
+                  label="Password"
+                  type="password"
+                  :required="Boolean(is_superuser)">
                 </v-text-field>
 
                 <v-text-field
@@ -49,7 +51,55 @@
                   label="Is an admin user">
                 </v-checkbox>
 
-                <v-btn :disabled="!valid" color="success" class="mr-4">Save</v-btn>
+                <v-btn
+                  class="mr-4"
+                  color="success"
+                  :disabled="!valid"
+                  @click.stop="dialog = true"
+                >
+                  Save
+                </v-btn>
+
+                <div class="text-center">
+                  <v-dialog
+                    v-model="dialog"
+                    persistent
+                    width="500"
+                  >
+
+                    <v-card>
+                      <v-card-title
+                        class="headline grey lighten-2"
+                      >
+                        Confirmation required
+                      </v-card-title>
+
+                      <v-card-text>
+                        Are you sure you want to save this record?
+                      </v-card-text>
+
+                      <v-divider></v-divider>
+
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn
+                          color="primary"
+                          text
+                          @click="dialog = false"
+                        >
+                          Cancel
+                        </v-btn>
+                        <v-btn
+                          color="green darken-1"
+                          text
+                          @click="save"
+                        >
+                          Save
+                        </v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+                </div>
               </v-form>
             </v-col>
           </v-row>
@@ -72,6 +122,14 @@ export default Vue.extend({
     Axios.defaults.headers = {
       Authorization: `Bearer ${sessionStorage.getItem('token')}`,
     };
+
+    if (this.is_superuser) {
+      this.passwordRules = [
+        (v: string) => !!v || 'Password is required',
+      ];
+    } else {
+      this.passwordRules = [];
+    }
 
     // load customers
     try {
@@ -104,36 +162,37 @@ export default Vue.extend({
     }
   },
 
+  mounted() {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (this.$refs.form as any).validate();
+  },
+
   data: () => ({
+    alert: {
+      text: '',
+      type: 'info',
+    },
     customer: '',
+    customerRules: [
+      (v: string) => !!v || 'Select a customer from the drop down',
+    ],
     customers: [{
       name: '',
       id: '',
       disabled: false,
     }],
-    alert: {
-      text: '',
-      type: 'info',
-    },
+    dialog: false,
     // eslint-disable-next-line @typescript-eslint/camelcase
     is_superuser: false,
-    valid: true,
-    name: '',
+    valid: false,
     maxEmailLength: MAX_EMAIL_LENGTH,
-    nameRules: [
-      (v: string) => !!v || 'Name is required',
-      (v: string) => (v && v.length <= MAX_EMAIL_LENGTH) || 'Name must be less than 10 characters',
-    ],
     email: '',
     emailRules: [
       (v: string) => !!v || 'E-mail is required',
       (v: string) => /.+@.+\..+/.test(v) || 'E-mail must be valid',
     ],
     password: '',
-    passwordRules: [
-      (v: string) => !!v || 'Password is required',
-    ],
-    select: null,
+    passwordRules: [] as Function[],
     checkbox: false,
     role: 'API User',
   }),
@@ -142,8 +201,40 @@ export default Vue.extend({
     customerList() {
       return this.customers.filter((item) => item.disabled === false);
     },
+    payload() {
+      const payload = {
+        email: this.email,
+        password: this.is_superuser ? this.password : '',
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        is_superuser: this.is_superuser,
+        customerId: this.customer,
+      };
+      return payload;
+    },
   },
 
-  methods: {},
+  methods: {
+    async save() {
+      const r = await Axios.put(`http://127.0.0.1:3000/users/${this.$route.params.id}`, this.payload);
+      const { data } = r;
+      this.dialog = false;
+      if (Object.keys(data).length) {
+        this.$router.push({ name: 'user-list' });
+      }
+    },
+  },
+
+  watch: {
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    is_superuser(val) {
+      if (val) {
+        this.passwordRules = [
+          (v: string) => !!v || 'Password is required',
+        ];
+      } else {
+        this.passwordRules = [];
+      }
+    },
+  },
 });
 </script>
